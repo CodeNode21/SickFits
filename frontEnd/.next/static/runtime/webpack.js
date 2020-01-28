91963188e568fe18da9d3aae62fe21147f6edbec
CodeNode21 <mbednara@gmail.com>
2020-01-27 23:57:31 -0500

    added babel dependencies

--- frontEnd/.next/static/runtime/webpack.js
+++ frontEnd/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "508afa2541a0e2dd0474";
+/******/ 	var hotCurrentHash = "704870b40c9a8ec50fb4";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
